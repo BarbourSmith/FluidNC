17b1fdc067ed5b18c4c0defd2afe370af725cebd
BarbourSmith <BarbourSmith@users.noreply.github.com>
2024-06-07 11:42:16 -0700

    Update GPIOPinDetail.cpp

--- FluidNC/src/Pins/GPIOPinDetail.cpp
+++ FluidNC/src/Pins/GPIOPinDetail.cpp
@@ -30,6 +30,8 @@ namespace Pins {
             case 6:
             case 7:
             case 8:
+            case 9:
+            case 10:
             case 11:
             case 12:
             case 13:
